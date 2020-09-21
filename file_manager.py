e1c4e62bc98f9ae2c5ae69751e17c7f2f5587403
Vladimir <gorunov341@yandex.ru>
2020-09-21 15:29:53 +0300

    gitignore

---
+++ file_manager.py
@@ -0,0 +1,14 @@
+
+class FileManager():
+
+    def __init__(self):
+        pass
+
+    def upload_file(self):
+        print('Upload file')
+    
+    def download(self):
+        print('Download file')
+
+    def delete_file(self):
+        print('Delete file')
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+import asyncio
 from aiohttp import web
+from file_manager import FileManager
 
 routes = web.RouteTableDef()
+fm = FileManager()
 
 @routes.get('/')
 async def hello(request):
-    return web.Response(text="Hello, world")
+    return web.Response(text="Async File Storage")
+
+@routes.post("/upload_file")
+async def upload_file(request):
+    fm.upload_file()
+
+@routes.get("/file/download")
+async def download_file(request):
+    fm.download()
+    return web.Response(text="Download File")
+
+@routes.delete("/file/delete")
+async def delete_file(request):
+    fm.delete_file()
 
 app = web.Application()
 app.add_routes(routes)
